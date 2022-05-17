c2e46fcd27878af3c82257f5935371b6532d9f94
Michael <micek.mk@mail.ru>
2022-05-17 10:37:41 +0300

    005 Решение принятое - 2*N^2 Каждый символ расцениваем как центр палиндрома и расходимся в сторону, проверяя, является ли подстрока ещё полиндромом или уже нет. Два однотипных цикла для чётного и нечетного палинждрома. подсмотренно тут https://habr.com/ru/post/276195/ф

--- 005_Longest_Palindromic_Substring/main.cpp
+++ 005_Longest_Palindromic_Substring/main.cpp
@@ -7,37 +7,37 @@ using namespace std;
 
 class Solution {
  public:
-  bool isPoli(string s) {
-    int size = s.size();
-    for (int i = 0; i < size / 2; ++i) {
-      if (s[i] != s[size - i - 1]) return false;
-    }
-
-    return true;
-  };
-
   int maxSize = 0;
   string out = "";
-  string longestPalindrome(string s) {
-    for (int i = 0; i < s.size(); ++i) {
-      char symbol = s[i];
-      if (s.size() - i <= maxSize) break;
-
-      int endstr = s.rfind(symbol);
-      while (endstr != i) {
-        if (endstr - i + 1 <= maxSize) break;
-        string subs = s.substr(i, endstr - i + 1);
-        if (isPoli(subs)) {
-          if (subs.size() > maxSize) {
-            maxSize = subs.size();
-            out = subs;
-          }
-          break;
-        } else
-          endstr = s.rfind(symbol, endstr - 1);
+  string longestPalindrome(string str) {
+    //для поиска нечётного палиндрома
+    for (int indMiddle = 0; indMiddle < str.length(); ++indMiddle) {
+      int leftBorder = indMiddle - 1, rightBorder = indMiddle + 1;
+      while (leftBorder >= 0 && rightBorder < str.length() &&
+             str[leftBorder] == str[rightBorder]) {
+        if (rightBorder - leftBorder > maxSize) {
+          maxSize = rightBorder - leftBorder;
+          out = str.substr(leftBorder, rightBorder - leftBorder + 1);
+        }
+        --leftBorder;
+        ++rightBorder;
       }
     }
-    if (out == "") out = s[0];
+
+    //для поиска чётно палиндрома
+    for (int indMiddle = 0; indMiddle < str.length(); ++indMiddle) {
+      int leftBorder = indMiddle, rightBorder = indMiddle + 1;
+      while (leftBorder >= 0 && rightBorder < str.length() &&
+             str[leftBorder] == str[rightBorder]) {
+        if (rightBorder - leftBorder > maxSize) {
+          maxSize = rightBorder - leftBorder;
+          out = str.substr(leftBorder, rightBorder - leftBorder + 1);
+        }
+        --leftBorder;
+        ++rightBorder;
+      }
+    }
+    if (out == "") out = str[0];
     return out;
   }
 };
@@ -45,21 +45,31 @@ class Solution {
 int main() {
   std::cout << "Hello World!\n" << std::endl;
   Solution S;
-  cout
-      << S.longestPalindrome(
-             "bkuadixhrtfehvbjjzojfyhuqyckdeilnftnklylyssbjivhvnplrzwrgcnrciypv"
-             "acbhdnglflipnlpzgivjfieunhzeaytshrintdwhbvbkhbsqbjxwhqrlneiwkmccd"
-             "nfcvapmtqihzyyoiaoqgtxkpbqqdboaxmmsdjxvdrwbhdnemqmsikoksfvjjovrbg"
-             "dtfgmhgryvvpunzrsluqzibsvyubyhqevpnfnszzriljpmoevpqacbvdcsgfzmnkh"
-             "nshsvynxxncqyjxqazcttkvjnkuvykgdrquybvlpwzladpetocuphzkgfuutqcbnt"
-             "twjmkkwbmbidcyauopcxmsarodcqabirbawtlgsytlflsiolxjghjmqrymadpzaet"
-             "cchyvkaezeavjemubbquclhcjcmbwxphllhfnzfyewpyyiirgnohylfbtfddeohti"
-             "frqiiwpdtyqjyemqjlnpcwlsylxjuxtnmcrpdznbzschmnjxnldxpkbrikpfsfwhs"
-             "arrfjueubvnztlwmognapvxwelyvueheqxtncpxhzwplaxqqrbmfmmqhohucxinxi"
-             "dxzhndvstideuwrisjgpwkgvsdxmlnfgqzzksflmjzckkyutrwptfvcawfbvqxlzt"
-             "stpoitdepexxiqtsdtjmssqbdinalsqkjjqkgilbfxajninuclquszwbmstcdbywf"
-             "hhnierqsegafyfqzvmqockcowfqwbgfxvdxbqobditvowhtdeptljetgj")
-      << endl;
+  cout << S.longestPalindrome(
+              "bkuadixhrtfehvbjjzojfyhuqyckdeilnftnklylyssbjivhvnplrzwrgcnrci"
+              "ypv"
+              "acbhdnglflipnlpzgivjfieunhzeaytshrintdwhbvbkhbsqbjxwhqrlneiwkm"
+              "ccd"
+              "nfcvapmtqihzyyoiaoqgtxkpbqqdboaxmmsdjxvdrwbhdnemqmsikoksfvjjov"
+              "rbg"
+              "dtfgmhgryvvpunzrsluqzibsvyubyhqevpnfnszzriljpmoevpqacbvdcsgfzm"
+              "nkh"
+              "nshsvynxxncqyjxqazcttkvjnkuvykgdrquybvlpwzladpetocuphzkgfuutqc"
+              "bnt"
+              "twjmkkwbmbidcyauopcxmsarodcqabirbawtlgsytlflsiolxjghjmqrymadpz"
+              "aet"
+              "cchyvkaezeavjemubbquclhcjcmbwxphllhfnzfyewpyyiirgnohylfbtfddeo"
+              "hti"
+              "frqiiwpdtyqjyemqjlnpcwlsylxjuxtnmcrpdznbzschmnjxnldxpkbrikpfsf"
+              "whs"
+              "arrfjueubvnztlwmognapvxwelyvueheqxtncpxhzwplaxqqrbmfmmqhohucxi"
+              "nxi"
+              "dxzhndvstideuwrisjgpwkgvsdxmlnfgqzzksflmjzckkyutrwptfvcawfbvqx"
+              "lzt"
+              "stpoitdepexxiqtsdtjmssqbdinalsqkjjqkgilbfxajninuclquszwbmstcdb"
+              "ywf"
+              "hhnierqsegafyfqzvmqockcowfqwbgfxvdxbqobditvowhtdeptljetgj")
+       << endl;
 
   return 0;
 }
